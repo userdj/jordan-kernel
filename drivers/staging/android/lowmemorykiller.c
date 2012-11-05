998080a9923f74e3a955aca2d8b41390706331c0
Blechd0se <alex.christ@hotmail.de>
2012-11-05 22:10:53 +0100

    Performance Tweaks

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -45,9 +45,9 @@ static int lowmem_adj[6] = {
 };
 static int lowmem_adj_size = 4;
 static size_t lowmem_minfree[6] = {
-	3 * 512,	/* 6MB */
+	3 * 768,	/* 6MB */
 	2 * 1024,	/* 8MB */
-	4 * 1024,	/* 16MB */
+	6 * 1024,	/* 16MB */
 	16 * 1024,	/* 64MB */
 };
 static int lowmem_minfree_size = 4;
