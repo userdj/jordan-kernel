fcfb9e520d569faa1a47c8165a48fdc257406a01
Quarx2k <agent00791@gmail.com>
2012-11-14 22:12:59 +0700

    update to 2.6.32-51

--- mm/memory.c
+++ mm/memory.c
@@ -2658,10 +2658,6 @@ static int do_anonymous_page(struct mm_struct *mm, struct vm_area_struct *vma,
 
 	pte_unmap(page_table);
 
-	/* Check if we need to add a guard page to the stack */
-	if (check_stack_guard_page(vma, address) < 0)
- 		return VM_FAULT_SIGBUS;
-
 	if (!(flags & FAULT_FLAG_WRITE)) {
 		entry = pte_mkspecial(pfn_pte(my_zero_pfn(address),
 						vma->vm_page_prot));
