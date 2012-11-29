271e810b9f7ecc650a650501c83b2b567d82768f
Blechd0se <alex.christ@hotmail.de>
2012-11-29 20:58:25 +0100

    More undervolting of display an camera

--- arch/arm/mach-omap2/board-mapphone-spi.c
+++ arch/arm/mach-omap2/board-mapphone-spi.c
@@ -199,10 +199,11 @@ static struct regulator_init_data cpcap_regulator[CPCAP_NUM_REGULATORS] = {
 		.num_consumer_supplies	= ARRAY_SIZE(cpcap_sw5_consumers),
 		.consumer_supplies	= cpcap_sw5_consumers,
 	},
+	// Should be Camera... Default was 2900000 (min, max)
 	[CPCAP_VCAM] = {
 		.constraints = {
-			.min_uV			= 2900000,
-			.max_uV			= 2900000,
+			.min_uV			= 2800000,
+			.max_uV			= 2800000,
 			.valid_ops_mask		= REGULATOR_CHANGE_STATUS,
 			.apply_uV		= 1,
 
@@ -249,8 +250,8 @@ static struct regulator_init_data cpcap_regulator[CPCAP_NUM_REGULATORS] = {
 	// should be Display, Orginal: 2775000(min, max)
 	[CPCAP_VHVIO] = {
 		.constraints = {
-			.min_uV			= 2575000,
-			.max_uV			= 2575000,
+			.min_uV			= 2475000,
+			.max_uV			= 2475000,
 			.valid_ops_mask		= REGULATOR_CHANGE_STATUS,
 			.always_on		= 1,
 			.apply_uV		= 1,
